8c4b8c7c6ce89b8da9738453ba531a4bbac162f1
ARJUN <ARJUN@DESKTOP-I3N79P6>
2022-06-08 22:17:10 +0530

    first commit

---
+++ wrapperclass/src/com/ait/wrapper/Example1.java
@@ -0,0 +1,11 @@
+package com.ait.wrapper;
+
+public class Example1 {
+// autoboxing
+	public static void main(String[] args) {
+		int a=10;
+		Integer j=a;
+		System.out.println(j);
+
+	}
+}
---
+++ wrapperclass/src/com/ait/wrapper/Example2.java
@@ -0,0 +1,13 @@
+package com.ait.wrapper;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Integer a=10;
+		int j=a;
+		int i=j;
+		System.out.println(a+" "+j+" "+i);
+
+	}
+
+}
